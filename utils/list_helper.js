4f53d4dff7cf27c62341f49c0a9b23a56799aaef
Tsaikkari <trospe@gmx.de>
2020-08-11 20:11:16 +0200

    Exercise 4.5

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -8,13 +8,21 @@ const totalLikes = (blogs) => {
   }
 
   let likes = blogs.map(blog => blog.likes)
-  console.log(likes)
   return blogs.length === 0
   ? 0
   : likes.reduce(reducer, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  let allLikes = blogs.map(blog => blog.likes)
+  console.log(allLikes)
+  let max = blogs.find(blog => blog.likes === Math.max(...allLikes))
+  console.log(max)
+  return max
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
