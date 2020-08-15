7b601b69f0f7e4b677ea3669f88c579b4701d7aa
Tsaikkari <trospe@gmx.de>
2020-08-16 00:58:29 +0200

    Exercise 4.6

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,15 +1,14 @@
 const _= require('lodash')
-const { filter } = require('lodash')
+
+const reducer = (sum, item) => {
+  return sum + item
+}
 
 const dummy = (blogs) => {
   return blogs.push(1)
 }
 
 const totalLikes = (blogs) => {
-  const reducer = (sum, item) => {
-    return sum + item
-  }
-
   let likes = blogs.map(blog => blog.likes)
   return blogs.length === 0
   ? 0
@@ -29,41 +28,27 @@ const mostBlogs = (blogs) => {
   for (const [author, arr] of Object.entries(authors)) {
     if (arr.length === max) {
     blogs.find(b => b === b.author === author)
-      return { author: author, blogs: max } 
+      return { author, blogs: max } 
     }
   }
 }
 
 const mostLikes = (blogs) => {
-  const reducer = (sum, item) => {
-    return sum + item
+  const likesGroup = _.groupBy(blogs, 'likes')
+  const authorGroup = _.groupBy(blogs, 'author')
+  console.log('likesGroup', likesGroup)
+  for (const [author, authorArr] of Object.entries(authorGroup)) {
+    const max = arr.find(a => a.totalLikes === Math.max.apply(null, arr))
+    console.log(max) // undefined
+    arr.find(a => a.author === author)
+    return { author, likes: max }
+    }
   }
-
-  const authorList = blogs.map((blog) => ({
-    author: blog.author,
-    likes: blog.likes
-  }))
-
-  const authors = authorList.map(a => a.author)
-  let uniqAuthors = authors.reduce((a, b) => {
-    if (a.indexOf(b) < 0) a.push(b)
-    return a
-  }, [])
-
-  // TODO: Fix this
-  let specificsAuthors = uniqAuthors.forEach(e => {
-    authorList.includes(e) 
-    && authorList.filter(a => a.author === uniqAuthor[e])
-  })
-
-  let likes = specificsAuthors.map(a => a.likes)
-  return likes.reduce(reducer, 0)
-}
 
 module.exports = {
   dummy,
   totalLikes,
   favoriteBlog,
   mostBlogs,
-  //mostLikes
+  mostLikes
 }
